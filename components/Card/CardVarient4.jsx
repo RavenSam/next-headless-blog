2757becfd983396b59cc42ca6b36ecd03b210299
Sam Makhlouf <maksam7dev@gmail.com>
2022-01-18 17:37:30 +0100

    images

--- components/Card/CardVarient4.jsx
+++ components/Card/CardVarient4.jsx
@@ -6,20 +6,20 @@ import trancate from "../../utils/trancate"
 export default function CardVarient4({ post, round, num }) {
    const m = imageUrl(post.featuredImage, "thumbnail")
 
-   console.log(m)
+   // console.log(post.featuredImage)
 
    return (
       <div className="group flex flex-col space-y-4 sm:space-y-0 sm:space-x-4 sm:flex-row">
          {round ? (
             <div className="relative img">
-               {/* <Image
+               <Image
                   className="w-full rounded-full"
-                  src={`${process.env.NEXT_PUBLIC_API_URL}${post.featuredImage.data.attributes.formats.thumbnail.url}`}
+                  src={imageUrl(post.featuredImage, "thumbnail")}
                   alt={post.title}
                   width={60}
                   height={60}
                   objectFit="cover"
-               /> */}
+               />
 
                {num && (
                   <span className="absolute -top-1 -left-1 text-white text-center leading-[2rem] w-8 h-8 rounded-full bg-sky-500 border border-white">
--- utils/imageUrl.js
+++ utils/imageUrl.js
@@ -1,5 +1,5 @@
 export default function imageUrl(featuredImage, format) {
-   const url = `${process.env.NEXT_PUBLIC_API_URL}${featuredImage.data.attributes.formats[format].url}`
+  const url = `${featuredImage.data.attributes.formats[format].url}`;
 
-   return url
+  return url;
 }
